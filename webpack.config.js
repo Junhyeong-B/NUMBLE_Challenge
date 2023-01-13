ead2b3cf2271de8cc27ccee68db1401e0030657c
Junhyeong-B <aowlr0513@gmail.com>
2023-01-14 02:05:39 +0900

    chore: ts-loader 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,59 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.ts",
-  mode: "development",
-  devtool: "inline-source-map",
+  entry: './src/index.ts',
+  mode: 'development',
+  devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      title: "넘블러 신년메세지 주고받기",
-      lang: "ko-KR",
+      title: '넘블러 신년메세지 주고받기',
+      lang: 'ko-KR',
       meta: {
-        description: "넘블러 신년메세지 주고받기 챌린지입니다..",
+        description: '넘블러 신년메세지 주고받기 챌린지입니다..',
       },
     }),
   ],
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
+        test: /\.tsx?$/i,
+        exclude: /node_modules/,
+        use: ['ts-loader'],
+      },
+      {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
-            plugins: ["@babel/plugin-proposal-object-rest-spread"],
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread'],
           },
         },
       },
     ],
   },
   resolve: {
-    extensions: [".ts", ".js"],
+    extensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    open: true,
+    port: 'auto',
   },
 };
