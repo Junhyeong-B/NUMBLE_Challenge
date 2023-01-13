e5bd3eaf8ccc92f7328ea842f9555bbf393e0cce
Junhyeong-B <aowlr0513@gmail.com>
2023-01-13 23:23:43 +0900

    chore: babel, html-plugin 설치

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
   entry: "./src/index.ts",
   mode: "development",
   devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "넘블러 신년메세지 주고받기",
+      lang: "ko-KR",
+      meta: {
+        description: "넘블러 신년메세지 주고받기 챌린지입니다..",
+      },
+    }),
+  ],
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
@@ -17,6 +27,17 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-object-rest-spread"],
+          },
+        },
       },
     ],
   },
